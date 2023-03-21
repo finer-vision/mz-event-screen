7b669174ce512cb0d89e30deaca22c61ac255c7f
FinerVision <finervision@pc-78-201.customer.ask4.lan>
2023-03-21 15:22:37 +0000

    chore: autoplay videos

--- src/pages/One/One.tsx
+++ src/pages/One/One.tsx
@@ -7,7 +7,6 @@ import video from "./video.webm"
 export default function One() {
     const {setBackground} = useBackground()
     const videoRef = useRef<HTMLVideoElement | null>(null)
-    const [playing, setPlaying] = useState(false)
 
     useEffect(() => {
         setBackground("black")
@@ -16,31 +15,9 @@ export default function One() {
     return (
         <Slide className="relative z-[60] flex flex-col justify-end items-center w-full h-full bg-cover bg-no-repeat">
             <video ref={videoRef}
-            onClick={() => {
-                console.log("click")
-                //if playing, pause, else play
-                if(playing) {
-                    videoRef.current?.pause()
-                } else {
-                    videoRef.current?.play()
-                }
-            }}
-            src={video} 
-            onPlay={() => {
-                setPlaying(true)
-            }}
-            onPause={() => {
-                setPlaying(false)
-            }}
-            onEnded={() => {
-                setPlaying(false)
-            }}
+            autoPlay
+            src={video}
              className="fixed z-10 top-0 left-0 w-screen h-screen"></video>
-            {!playing && <button 
-            className="w-[20%] aspect-sqaure absolute z-20 left-1/2 top-[35%] -translate-x-1/2 pointer-events-none">
-                <img className="w-full h-full"
-                src="./page1playbtn.svg"/>
-            </button>}
         </Slide>
     )
 }

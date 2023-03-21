a9dffe4270ed5167d09673fcef9d25c3a7b9fc77
FinerVision <finervision@pc-78-201.customer.ask4.lan>
2023-03-21 16:00:32 +0000

    chore: fix pill buttons

--- src/pages/Four/Button.tsx
+++ src/pages/Four/Button.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 export default function Button({children, to}: {children: React.ReactNode, to: string}) {
     return (
         <Link to={to}
-        className="text-center cursor-pointer uppercase rounded-full px-[3vw] text-white  overflow-hidden relative bg-gradient-to-r from-[#05CCA3] via-[#46AFE1] to-[#8085F0] text-[1cqh] grid place-items-center">
+        className="text-center cursor-pointer uppercase rounded-full px-[3vw] text-white  overflow-hidden relative bg-gradient-to-r from-[#05CCA3] via-[#46AFE1] to-[#8085F0] text-[2vw] grid place-items-center">
             {children}
         </Link>
     )

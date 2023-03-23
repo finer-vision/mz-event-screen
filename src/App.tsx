c268d2fc40405826e59d82eed83bc4305b807c7c
axseinga <urbanowicz.uk@gmail.com>
2023-03-23 17:30:04 +0000

    chore: add intro video

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { Routes, Route, useNavigate } from "react-router-dom";
-import One from "@/pages/One/One"
-import Two from "@/pages/Two/Two"
-import Three from "@/pages/Three/Three"
-import Four from "@/pages/Four/Four"
-import Five from "@/pages/Five/Five"
+import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
+import Intro from "./pages/intro/intro";
+import One from "@/pages/One/One";
+import Two from "@/pages/Two/Two";
+import Three from "@/pages/Three/Three";
+import Four from "@/pages/Four/Four";
+import Five from "@/pages/Five/Five";
 import { AnimatePresence } from "framer-motion";
 import { useBackground } from "./stores";
 import { useIdleTimer } from "react-idle-timer";
@@ -12,34 +13,40 @@ export const DEV = import.meta.env.MODE === "development";
 
 export default function App() {
   const navigate = useNavigate();
-  const { background } = useBackground()
+  const location = useLocation();
+  const { background } = useBackground();
+
+  const path = location.pathname;
 
   useIdleTimer({
     timeout: 60000,
     onIdle: () => {
-      navigate("/1")
-    }
-  })
+      navigate("/1");
+    },
+  });
 
   return (
     <AnimatePresence>
-      <div style={{ background }}
-      className="absolute top-0 left-0 w-screen h-screen flex flex-col overflow-hidden">
-        <div onClick={(e) => {
+      <div
+        style={{ background }}
+        className="absolute top-0 left-0 w-screen h-screen flex flex-col overflow-hidden"
+      >
+        <div
+          onClick={(e) => {
             e.stopPropagation();
-            navigate("/")
+            navigate("/");
           }}
-        className="w-full grid place-items-center mt-[12%] relative z-[60]">
-          <img
-          className="w-[46%]" src="./logo.svg"/>
+          className="w-full grid place-items-center mt-[12%] relative z-[60]"
+        >
+          {path === "/" ? <></> : <img className="w-[46%]" src="./logo.svg" />}
         </div>
         <Routes key={1}>
-          <Route path="/" element={<One/>} />
-          <Route path="/1" element={<One/>} />
-          <Route path="/2" element={<Two/>} />
-          <Route path="/3" element={<Three/>} />
-          <Route path="/4/:title" element={<Four/>} />
-          <Route path="/5/:title/:id" element={<Five/>} />
+          <Route path="/" element={<Intro />} />
+          <Route path="/1" element={<One />} />
+          <Route path="/2" element={<Two />} />
+          <Route path="/3" element={<Three />} />
+          <Route path="/4/:title" element={<Four />} />
+          <Route path="/5/:title/:id" element={<Five />} />
         </Routes>
       </div>
     </AnimatePresence>
---
+++ src/pages/intro/intro.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import video from "./video.mp4";
+import { useNavigate } from "react-router-dom";
+
+export default function Intro() {
+  const navigate = useNavigate();
+
+  const handleNextPage = () => {
+    navigate("/1");
+  };
+
+  return (
+    <div className="fixed inset-0 z-50 bg-black">
+      <video
+        src={video}
+        muted
+        playsInline
+        autoPlay
+        onEnded={handleNextPage}
+        controlsList="nodownload"
+        onContextMenu={(e) => e.preventDefault()}
+        className="w-full h-full object-contain"
+      />
+    </div>
+  );
+}

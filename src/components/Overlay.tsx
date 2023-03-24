4028ccfa930d467cda22b85414cbb5a52095b577
axseinga <urbanowicz.uk@gmail.com>
2023-03-24 10:33:24 +0000

    chore: add QR code

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -1,12 +1,22 @@
+import { useLocation } from "react-router-dom";
 
 type OverlayProps = {
-    handleClick?: () => void;
-}
+  handleClick?: () => void;
+};
 
-export default function Overlay({handleClick}: OverlayProps) {
-    return (
-        <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-between z-[60]" onClick={handleClick}>
-            <div className="fixed z-50 bottom-0 w-full h-[1%] bg-gradient-to-r from-[#05CCA3] via-[#46AFE1] to-[#8085F0]"></div>
-        </div>
-    )
+export default function Overlay({ handleClick }: OverlayProps) {
+  const location = useLocation();
+  const path = location.pathname;
+
+  return (
+    <div
+      className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-between z-[60]"
+      onClick={handleClick}
+    >
+      {path !== "intro" && path !== "/1" && path !== "/2" ? (
+          <img src="./QR-code.svg" alt="QR Code" className="self-end"/>
+      ) : <></>}
+      <div className="fixed z-50 bottom-0 w-full h-[1%] bg-gradient-to-r from-[#05CCA3] via-[#46AFE1] to-[#8085F0]"></div>
+    </div>
+  );
 }

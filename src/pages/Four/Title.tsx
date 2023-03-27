825e963447350ae2101f61eca34b078f0ec56681
Yahya <yks@dr.com>
2023-03-27 16:44:18 +0100

    chore: Increase font size in title pill outline

--- src/pages/Four/Title.tsx
+++ src/pages/Four/Title.tsx
@@ -5,7 +5,7 @@ export default function Title({children}: {children: React.ReactNode}) {
         <div style={{
             backgroundImage: `url(${titleBg})`,
         }}
-        className="text-center uppercase bg-[100%,100%] bg-no-repeat bg-center text-white min-h-[4vh] max-h-[4vh] -mb-[2vw] grid place-items-center text-[1.4vw]">
+        className="text-center uppercase bg-[100%,100%] bg-no-repeat bg-center text-white min-h-[4vh] max-h-[4vh] -mb-[2vw] grid place-items-center text-[2vw]">
             {children}
         </div>
     )

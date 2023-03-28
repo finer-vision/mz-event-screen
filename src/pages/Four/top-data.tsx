ec8a841707b92196c59326f89a3abde57b710171
Yahya <yks@dr.com>
2023-03-28 15:15:46 +0100

    chore: update bullet points

--- src/pages/Four/top-data.tsx
+++ src/pages/Four/top-data.tsx
@@ -2,90 +2,57 @@ export default {
   public_trust: {
     content: (
       <>
-        <h2 className="my-5 w-full text-[1.5vw]">
-          We understand the psychology of trust, how to build it and maintain
-          it. There is no time like now.
-        </h2>
-        <div className="flex flex-col gap-10 text-[1.3vw]">
-          <ul>
-            <li>
-              - Emotive content - films, podcasts, digital and experiential
-            </li>
-            <li>- Branded Content</li>
-            <li>- Crisis comms</li>
-            <li>- News announcements</li>
-          </ul>
-          <ul>
-            <li>- Media training</li>
-            <li>- Reputation management</li>
-            <li>- Storytelling courses</li>
-            <li>- Learning</li>
-          </ul>
+        <div className="text-[#05CFA6] font-bold flex flex-col text-[2vw] w-full">
+          <div>Emotive content films, podcasts, digital and experiential</div>
+          <div> Branded Content</div>
+          <div> Crisis comms</div>
+          <div> News announcements</div>
+          <div> Media training</div>
+          <div> Reputation management</div>
+          <div> Storytelling courses</div>
+          <div> Learning</div>
         </div>
       </>
     ),
   },
   right_people: {
-    content: <>
-        <h2 className="my-5 w-full text-[1.5vw]">
-            Get noticed by the people you want and set them off to the best start!
-        </h2>
-        <div className="flex flex-col gap-10 text-[1.3vw]">
-          <ul>
-            <li>
-              - Recruitment advertising
-            </li>
-            <li>- Onboarding e-platforms, communications  and learning </li>
-          </ul>
+    content: (
+      <>
+        <div className="text-[#05CFA6] font-bold flex flex-col text-[2vw] w-full">
+          <div>Recruitment advertising</div>
+          <div> Onboarding e-platforms, communications and learning </div>
         </div>
-    </>,
+      </>
+    ),
   },
   working_cultures: {
-    content: <>
-        <h2 className="my-5 w-full text-[1.5vw]">
-        We know this is a priority across government and our expertise has already helped a number of government departments to see positive, measurable change. 
-        </h2>
-        <div className="flex flex-col gap-10 text-[1.3vw]">
-          <ul>
-            <li>
-              - Insights reports and Discovery workshops
-            </li>
-            <li>- Internal Communications</li>
-            <li>- Building Networks</li>
-            <li>- Diversity champions / influencers</li>
-          </ul>
-          <ul>
-            <li>
-                - Bespoke Learning 
-                <ul className="ml-[2vw]">
-                    <li>- Microaggressions</li>
-                    <li>- Bullying</li>
-                    <li>- Disability awareness</li>
-                    <li>- Speak Up programmes</li>
-                    <li>- Unconscious Bias</li>
-                </ul>
-            </li>
-          </ul>
+    content: (
+      <>
+        <div className="text-[#05CFA6] font-bold flex flex-col text-[2vw] w-full">
+          <div>Insights reports and Discovery workshops</div>
+          <div> Internal Communications</div>
+          <div> Building Networks</div>
+          <div> Diversity champions / influencers</div>
+          <div> Microaggressions</div>
+          <div> Bullying</div>
+          <div> Disability awareness</div>
+          <div> Speak Up programmes</div>
+          <div> Unconscious Bias</div>
         </div>
-    </>,
+      </>
+    ),
   },
   collaborate_effectively: {
-    content: <>
-    
-    <h2 className="my-5 w-full text-[1.5vw]">
-    We can help you build effective cross-department and cross-government working practices.
-        </h2>
-        <div className="flex flex-col gap-10 text-[1.3vw]">
-          <ul>
-            <li>
-              - Leadership workshops
-            </li>
-            <li>- Learning programmes</li>
-            <li>- Crisis comms</li>
-            <li>- Creative consulting - creative thinking for problem solving</li>
-            <li>- Internal Communications</li>
-          </ul>
+    content: (
+      <>
+        <div className="text-[#05CFA6] font-bold flex flex-col text-[2vw] w-full">
+          <div>Leadership workshops</div>
+          <div> Learning programmes</div>
+          <div> Crisis comms</div>
+          <div> Creative consulting creative thinking for problem solving</div>
+          <div> Internal Communications</div>
         </div>
-    </>,
+      </>
+    ),
   },
-} as any
+} as any;

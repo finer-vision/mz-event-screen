5bba5d7d5c983c2b797bd935c1a8b9a1845909b9
Enijar <TheEnijar@gmail.com>
2023-09-12 16:24:34 +0100

    chore: update home hero

--- src/pages/Four/Four.tsx
+++ src/pages/Four/Four.tsx
@@ -1,9 +1,9 @@
 import Logos from "@/components/Logos";
 import Carousel from "./Carousel";
 import { useBackground } from "@/stores";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import axios from "axios";
-import { useNavigate, useParams } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import Button from "./Button";
 import Title from "./Title";
 import Overlay from "../../components/Overlay";
@@ -15,7 +15,7 @@ const fullTitles = {
   page1: `Induction &\nOnboarding`,
   page2: `Transformation &\nInnovation`,
   page3: `Mandatory &\nESG`,
-  page4: `DEIB &\nLeadership`
+  page4: `DEIB &\nLeadership`,
 };
 
 const URL = import.meta.env.VITE_GH_PAGES === "TRUE" ? "/mz-event-screen/" : "";
@@ -23,13 +23,14 @@ const URL = import.meta.env.VITE_GH_PAGES === "TRUE" ? "/mz-event-screen/" : "";
 export default function Four() {
   const { title } = useParams();
   const { setBackground } = useBackground();
+  const { pathname } = useLocation();
   const [items, setItems] = useState([]);
   const navigate = useNavigate();
 
   useEffect(() => {
     axios.get(`${URL}data/${title}/${title}.json`).then((res) => {
       setItems(res.data);
-      console.log(res.data)
+      console.log(res.data);
     });
   }, [title]);
 
@@ -67,6 +68,50 @@ export default function Four() {
     setBackground("black");
   }, []);
 
+  const header = useMemo(() => {
+    if (pathname.endsWith("page1")) {
+      return (
+        <>
+          <div className="w-full flex flex-col px-[10%] gap-[5vw]">
+            <div className="w-10/12 flex">
+              <button
+                onClick={() => {
+                  navigate(`/3`);
+                }}
+                className="text-[3vw] px-[2vw] rounded-full bg-gradient-to-r from-[#05CCA3] via-[#46AFE1] to-[#8085F0]"
+              >
+                BACK
+              </button>
+            </div>
+            <Title>{(fullTitles as any)[title as any]}</Title>
+            <div className="w-[68vw] h-[16vh] ml-[10.5vw]">
+              <img src="./hero.png" alt="" />
+            </div>
+          </div>
+        </>
+      );
+    }
+    return (
+      <>
+        <div className="w-full flex flex-col px-[10%] gap-[5vw]">
+          <div className="w-10/12 flex">
+            <button
+              onClick={() => {
+                navigate(`/3`);
+              }}
+              className="text-[3vw] px-[2vw] rounded-full bg-gradient-to-r from-[#05CCA3] via-[#46AFE1] to-[#8085F0]"
+            >
+              BACK
+            </button>
+          </div>
+          <Title>{(fullTitles as any)[title as any]}</Title>
+          <div className="w-full h-[16vh]">{topData[title as any].content}</div>
+        </div>
+        <img className="w-[40vw]" src={`./${title}.png`} />
+      </>
+    );
+  }, [pathname]);
+
   if (!items.length) return <>Loading...</>;
 
   return (
@@ -77,39 +122,39 @@ export default function Four() {
           className="w-full flex flex-col grow gap-[5%] mt-[10%]"
         >
           <div className="w-full grid grid-cols-2 place-content-center place-items-center">
-            <div className="w-full flex flex-col px-[10%] gap-[5vw]">
-              <div className="w-10/12 flex">
-                <button
-                  onClick={() => {
-                    navigate(`/3`);
-                  }}
-                  className="text-[3vw] px-[2vw] rounded-full bg-gradient-to-r from-[#05CCA3] via-[#46AFE1] to-[#8085F0]"
-                >
-                  BACK
-                </button>
-              </div>
-              <Title>{(fullTitles as any)[title as any]}</Title>
-              <div className="w-full h-[16vh]">
-                {topData[title as any].content}
-              </div>
-            </div>
-            <img className="w-[40vw]" src={`./${title}.png`} />
+            {header}
           </div>
           <Carousel items={items} />
           <div className="grid grid-cols-2 w-full px-[3vw] gap-[3vw] h-[3vh] place-items-center place-content-center w-[72%] mx-auto">
             <div className="ml-auto">
-              <Button width={20} py={3} iconDirection="left" to={`/4/${prevPageId}`}>
-              <span className="whitespace-pre py-[1vw]" style={{lineHeight: "1", display:"block"}}>
-                {prevPageTitle}
-              </span>
+              <Button
+                width={20}
+                py={3}
+                iconDirection="left"
+                to={`/4/${prevPageId}`}
+              >
+                <span
+                  className="whitespace-pre py-[1vw]"
+                  style={{ lineHeight: "1", display: "block" }}
+                >
+                  {prevPageTitle}
+                </span>
               </Button>
             </div>
             <div className="mr-auto">
-            <Button width={20} py={3} iconDirection="right" to={`/4/${nextPageId}`}>
-              <span className="whitespace-pre py-[1vw]" style={{lineHeight: "1", display:"block"}}>
-                {nextPageTitle}
-              </span>
-            </Button>
+              <Button
+                width={20}
+                py={3}
+                iconDirection="right"
+                to={`/4/${nextPageId}`}
+              >
+                <span
+                  className="whitespace-pre py-[1vw]"
+                  style={{ lineHeight: "1", display: "block" }}
+                >
+                  {nextPageTitle}
+                </span>
+              </Button>
             </div>
           </div>
         </motion.div>

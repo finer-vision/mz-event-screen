a6c2636c657b508c70de0d31d6b6dab8682c1714
Enijar <TheEnijar@gmail.com>
2023-09-13 10:16:23 +0100

    chore: event stand amends 3

--- src/pages/Five/Title.tsx
+++ src/pages/Five/Title.tsx
@@ -1,12 +1,7 @@
-import titleBg from "./TitleBg.svg"
-
-export default function Title({children}: {children: React.ReactNode}) {
-    return (
-        <div style={{
-            backgroundImage: `url(${titleBg})`,
-        }}
-        className="font-bold text-center uppercase bg-[100%,100%] bg-no-repeat bg-center text-white grid place-items-center w-[50vw] px-[1vw] py-[4vw] text-[2.4vw]">
-            {children}
-        </div>
-    )
+export default function Title({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="font-bold text-center uppercase bg-[100%,100%] bg-no-repeat bg-center text-white grid place-items-center w-[50vw] px-[1vw] py-[4vw] text-[2.4vw]">
+      {children}
+    </div>
+  );
 }

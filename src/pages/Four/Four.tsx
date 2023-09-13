464429a8918c43caad87b98d4864ae07cf7157dc
Enijar <TheEnijar@gmail.com>
2023-09-13 12:09:20 +0100

    chore: event stand amends 4

--- src/pages/Four/Four.tsx
+++ src/pages/Four/Four.tsx
@@ -1,9 +1,9 @@
 import Logos from "@/components/Logos";
 import Carousel from "./Carousel";
 import { useBackground } from "@/stores";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
-import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import Button from "./Button";
 import Title from "./Title";
 import Overlay from "../../components/Overlay";
@@ -23,7 +23,6 @@ const URL = import.meta.env.VITE_GH_PAGES === "TRUE" ? "/mz-event-screen/" : "";
 export default function Four() {
   const { title } = useParams();
   const { setBackground } = useBackground();
-  const { pathname } = useLocation();
   const [items, setItems] = useState([]);
   const navigate = useNavigate();
 
@@ -68,50 +67,6 @@ export default function Four() {
     setBackground("black");
   }, []);
 
-  const header = useMemo(() => {
-    if (pathname.endsWith("page1")) {
-      return (
-        <>
-          <div className="w-full flex flex-col px-[10%] gap-[5vw]">
-            <div className="w-10/12 flex">
-              <button
-                onClick={() => {
-                  navigate(`/3`);
-                }}
-                className="text-[3vw] px-[2vw] rounded-full bg-gradient-to-r from-[#05CCA3] via-[#46AFE1] to-[#8085F0]"
-              >
-                BACK
-              </button>
-            </div>
-            <Title>{(fullTitles as any)[title as any]}</Title>
-            <div className="w-[68vw] h-[16vh] ml-[10.5vw]">
-              <img src="./hero.png" alt="" />
-            </div>
-          </div>
-        </>
-      );
-    }
-    return (
-      <>
-        <div className="w-full flex flex-col px-[10%] gap-[5vw]">
-          <div className="w-10/12 flex">
-            <button
-              onClick={() => {
-                navigate(`/3`);
-              }}
-              className="text-[3vw] px-[2vw] rounded-full bg-gradient-to-r from-[#05CCA3] via-[#46AFE1] to-[#8085F0]"
-            >
-              BACK
-            </button>
-          </div>
-          <Title>{(fullTitles as any)[title as any]}</Title>
-          <div className="w-full h-[16vh]">{topData[title as any].content}</div>
-        </div>
-        <img className="w-[40vw]" src={`./${title}.png`} />
-      </>
-    );
-  }, [pathname]);
-
   if (!items.length) return <>Loading...</>;
 
   return (
@@ -122,7 +77,21 @@ export default function Four() {
           className="w-full flex flex-col grow gap-[5%] mt-[10%]"
         >
           <div className="w-full grid grid-cols-2 place-content-center place-items-center">
-            {header}
+            <div className="w-full flex flex-col px-[10%] gap-[5vw]">
+              <div className="w-10/12 flex">
+                <button
+                    onClick={() => {
+                      navigate(`/3`);
+                    }}
+                    className="text-[3vw] px-[2vw] rounded-full bg-gradient-to-r from-[#05CCA3] via-[#46AFE1] to-[#8085F0]"
+                >
+                  BACK
+                </button>
+              </div>
+              <Title>{(fullTitles as any)[title as any]}</Title>
+              <div className="w-full h-[16vh]">{topData[title as any].content}</div>
+            </div>
+            <img className="w-[40vw]" src={`./${title}.png`} />
           </div>
           <Carousel items={items} />
           <div className="grid grid-cols-2 w-full px-[3vw] gap-[3vw] h-[3vh] place-items-center place-content-center w-[72%] mx-auto">
